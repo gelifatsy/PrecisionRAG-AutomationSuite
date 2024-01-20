0dd94e20f46fc68fd3cad362c63e76f9bc3a49e3
gelifatsy <elias.assamnew@gmail.com>
2024-01-20 14:41:19 +0300

    test data generation

---
+++ Scripts/user_input.py
@@ -0,0 +1,9 @@
+
+
+def get_user_input():
+    user_input = input("Enter your input: ")
+    return user_input
+
+# Call the function to get user input
+user_input = get_user_input()
+print("User input:", user_input)
